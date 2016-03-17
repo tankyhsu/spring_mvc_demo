81c296bcd715e2ec9156b37f695f3f625ee4e281
xutq <xutq@inspur.com>
2016-03-17 10:09:57 +0800

    Spring MVC + AngularJS demo

---
+++ src/main/java/com/springmvc/model/Book.java
@@ -0,0 +1,45 @@
+package com.springmvc.model;
+
+/**
+ * Created by Administrator on 2016/2/29.
+ */
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(int id, String name, String author) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
---
+++ src/main/webapp/static/js/app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Administrator on 2016/3/1.
+ */
+'use strict';
+
+var App = angular.module('myApp', []);
